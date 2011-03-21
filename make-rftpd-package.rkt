e04a3845dd4530ed30235cf4667286234e7e1c80
netluxe <netluxe@1cd57258-c12c-8fd4-9513-a572fc92233d>
2011-03-21 23:45:35 +0000

    Update

---
+++ make-rftpd-package.rkt
@@ -0,0 +1,88 @@
+#|
+Summary:
+This file is part of Racket FTP Server.
+
+License:
+Copyright (c) 2010-2011 Mikhail Mosienko <netluxe@gmail.com>
+All Rights Reserved
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+|#
+
+#lang racket
+
+(require compiler/embed)
+(require compiler/distribute)
+
+(define package-dir "rftpd-package")
+(define temp-dir (find-system-path 'temp-dir))
+(define dest-exe (build-path temp-dir "rftpd.exe"))
+(define logs-dir (build-path package-dir "logs"))
+(define certs-src "certs")
+(define certs-dest (build-path package-dir "certs"))
+(define conf-src "conf")
+(define conf-dest (build-path package-dir "conf"))
+(define license-src "LICENSE")
+(define license-dest (build-path package-dir "LICENSE"))
+(define copyright-src "COPYRIGHT")
+(define copyright-dest (build-path package-dir "COPYRIGHT"))
+(define flags "flags.rkt")
+
+(display "Please wait for the package build")
+(letrec ([loop (λ() 
+                 (sleep 0.5) 
+                 (write-char #\.)
+                 (flush-output)
+                 (loop))])
+  (thread loop)
+  (void))
+
+(when (file-exists? dest-exe)
+  (delete-file dest-exe))
+
+(when (directory-exists? package-dir)
+  (delete-directory/files package-dir))
+
+(make-directory package-dir)
+(make-directory logs-dir)
+(copy-directory/files conf-src conf-dest)
+(copy-directory/files certs-src certs-dest)
+(copy-file license-src license-dest)
+(copy-file copyright-src copyright-dest)
+
+(display-lines-to-file
+ '("#lang racket/base"
+   "(require (for-syntax racket/base))"
+   "(provide (for-syntax create-executable?))"
+   "(define-for-syntax create-executable? #t)")
+ flags
+ #:exists 'truncate)
+
+(with-handlers ([void displayln])
+  (create-embedding-executable 
+   dest-exe
+   #:modules '((#f (file "rftpd.rkt")))
+   #:literal-expression
+   (parameterize ([current-namespace (make-base-namespace)])
+     (compile '(namespace-require ''rftpd))))
+  (assemble-distribution package-dir (list dest-exe)))
+
+(delete-file dest-exe)
+(display-lines-to-file
+ '("#lang racket/base"
+   "(require (for-syntax racket/base))"
+   "(provide (for-syntax create-executable?))"
+   "(define-for-syntax create-executable? #f)")
+ flags
+ #:exists 'truncate)
